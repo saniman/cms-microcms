7d21561fdd13162e8db48c49bbe6b784543038bd
saniman <saniman32@gmail.com>
2020-09-13 20:43:08 +0900

    pagesの中身をもと戻す

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
+import Container from "@/components/container";
+import MoreStories from "@/components/more-stories";
+import HeroPost from "@/components/hero-post";
+import Intro from "@/components/intro";
+import Layout from "@/components/layout";
+import { getAllPostsForHome } from "@/lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "@/lib/constants";
 
-
-export default function Index({  }) {
-  
+export default function Index({ allPosts }) {
+  const heroPost = allPosts.contents[0];
+  const morePosts = allPosts.contents.slice(1);
   return (
     <>
-      
+      <Layout>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+              id={heroPost.id}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
     </>
   );
 }
 
-
+export async function getStaticProps({ preview = false }) {
+  const allPosts = (await getAllPostsForHome(preview)) || [];
+  return {
+    props: { allPosts },
+  };
+}
