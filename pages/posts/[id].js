0d7a1b917e5862a460963614ca7a74209d421958
saniman <saniman32@gmail.com>
2020-09-13 20:41:19 +0900

    一旦pagesの中身削除

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,74 +1,10 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Container from '@/components/container'
-import PostBody from '@/components/post-body'
-import MoreStories from '@/components/more-stories'
-import Header from '@/components/header'
-import PostHeader from '@/components/post-header'
-import SectionSeparator from '@/components/section-separator'
-import Layout from '@/components/layout'
-import { getAllPostsWithID, getPostAndMorePosts } from '@/lib/api'
-import PostTitle from '@/components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from '@/lib/constants'
-import markdownToHtml from '@/lib/markdownToHtml'
 
-export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
+
+export default function Post({ }) {
+  
   return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article>
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.coverImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-            <SectionSeparator />
-            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-          </>
-        )}
-      </Container>
-    </Layout>
+   
+    <div>test</div>
   )
 }
 
-export async function getStaticProps({ params, previewData, preview = false }) {
-  const data = await getPostAndMorePosts(params.id, previewData?.draftKey)
-  const content = await markdownToHtml(data?.postByID.content || '')
-  return {
-    props: {
-      preview,
-      post: {
-        ...data?.postByID,
-        content,
-      },
-      morePosts: data?.morePosts.contents ?? [],
-    },
-  }
-}
-
-export async function getStaticPaths() {
-  const allPosts = await getAllPostsWithID()
-  return {
-    paths: allPosts?.map((post) => `/posts/${post.id}`) || [],
-    fallback: true,
-  }
-}
