aef5352007ddfd56ab3da905232753324a08d362
saniman <saniman32@gmail.com>
2020-09-15 08:57:08 +0900

    DatoCMSの設定が入ってたので削除

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,16 +1,8 @@
 import { getPreviewPostByID } from '@/lib/api'
 
 export default async function preview(req, res) {
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  if (
-    req.query.secret !== process.env.DATOCMS_PREVIEW_SECRET ||
-    !req.query.id
-  ) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
-
-  // Fetch the headless CMS to check if the provided `slug` exists
+  
+  // Fetch the headless CMS to check if the provided `id` exists
   const post = await getPreviewPostByID(req.query.id,req.query.draftKey)
   console.log(post)
   // If the slug doesn't exist prevent preview mode from being enabled
